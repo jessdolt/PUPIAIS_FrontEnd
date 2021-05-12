f5399367afd4224ed09ef46e3dbda5806c00f40d
JamesMargaux <margaux.padiwan23@gmail.com>
2021-05-12 21:24:23 +0800

    alertModal updated

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -5,55 +5,29 @@ const alertModalObj = {
         const btnLogout = document.querySelector(".adminNav button")
         const btnDelete = document.querySelector(".deleteSelected");
         const btnDeleteInline = document.querySelector(".btnDeleteInline");
+        const modalBackground = document.querySelector(".alertModalContainer");
+        const modalLogout = document.querySelector(".logoutAlert");
+        const modalDelete = document.querySelector(".deleteAlert");
+        const modalBtnCancel = document.querySelectorAll(".alertModal div button");
         //--------------------Event Listeners------------------------
-        btnLogout.addEventListener("click", generateModal);
-        btnDelete.addEventListener("click", generateModal);
-        btnDeleteInline.addEventListener("click", generateModal);
+        btnLogout.addEventListener("click", generateLogoutModal);
+        btnDelete.addEventListener("click", generateDeleteModal);
+        btnDeleteInline.addEventListener("click", generateDeleteModal);
         //--------------------functions------------------------
-        function generateModal(e){
-            let targetElem = e.target;
-            const modalBackground = document.createElement("div");
-            const modal = document.createElement("div");
-            const modalSvg = document.createElement("svg");
-            const modalHeader = document.createElement("h2");
-            const modalPara = document.createElement("p");
-            const modalBtnContainer = document.createElement("div");
-            const modalBtnCancel = document.createElement("button");
-            const modalBtnDelete = document.createElement("button");
-
-            modalBackground.classList.add("alertModalContainer", "show");
-            do{
-                if(targetElem == btnDelete || targetElem == btnDeleteInline){
-                    modal.classList.add("alertModal","deleteAlert");
-                    modalHeader.textContent = "Are you sure?";
-                    modalBtnDelete.textContent = "Yes! Delete";
-                }
-                else if(targetElem == btnLogout){
-                    modal.classList.add("alertModal","logoutAlert");
-                    modalHeader.textContent = "Are you leaving?";
-                    modalBtnDelete.textContent = "Logout";
-                }
-                targetElem = targetElem.parentNode;
-            }while(targetElem);
-            
-            modalPara.textContent = "This will delete all the data selected and cannot be undone!";
-            modalBtnCancel.textContent = "Cancel";
-
-            modalBackground.appendChild(modal);
-            modal.appendChild(modalSvg);
-            modal.appendChild(modalHeader);
-            modal.appendChild(modalPara);
-            modal.appendChild(modalBtnContainer);
-            modalBtnContainer.appendChild(modalBtnCancel);
-            modalBtnContainer.appendChild(modalBtnDelete);
-
-            modalBtnCancel.onclick = () => {
-                modalBackground.remove();
-            }
-
-            body.appendChild(modalBackground);
+        modalBtnCancel.forEach(element => element.onclick = () => {
+            modalBackground.classList.remove("show");
+            modalDelete.classList.remove("show");
+            modalBackground.classList.remove("show");
+            modalLogout.classList.remove("show");
+        })
+        function generateDeleteModal(){
+            modalBackground.classList.add("show");
+            modalDelete.classList.add("show");
         }
-
+        function generateLogoutModal(){
+            modalBackground.classList.add("show");
+            modalLogout.classList.add("show");
+        }
     }
 }
 
