ed567bb16f43d422b237f025aa2001b4bebccbbc
JamesMargaux <margaux.padiwan23@gmail.com>
2021-06-06 21:54:06 +0800

    forum update with js

---
+++ assets/js/forum.js
@@ -0,0 +1,26 @@
+const forum = {
+    init:() => {
+        /* const replyBtn = document.querySelectorAll(".reply") */
+        const cancelReply = document.querySelectorAll(".btn-con2 > .cancel")
+        const submitReply = document.querySelectorAll(".btn-con2 > .submit-reply")
+
+        if(replyBtn){
+            /* replyBtn.forEach(element => element.addEventListener("click", ()=>{
+                let parentli = element.parentElement.parentElement.parentElement
+                parentli.classList.add("active")
+            })) */
+
+            cancelReply.forEach(element => element.addEventListener("click", ()=>{
+                let parentli = element.parentElement.parentElement.parentElement
+                parentli.classList.remove("active")
+            }))
+
+            submitReply.forEach(element => element.addEventListener("click", ()=>{
+                let parentli = element.parentElement.parentElement.parentElement
+                parentli.classList.remove("active")
+            }))
+        }
+    }
+}
+
+export default forum
--- assets/js/index.js
+++ assets/js/index.js
@@ -3,12 +3,14 @@ import alertModalObj from './modal'
 import table from './table'
 import inputUtil from './inputFunctions'
 import card from './card'
+import forum from './forum'
 
 nav.init();
 alertModalObj.init();
 table.init();
 inputUtil.init();
 card.init();
+forum.init();
 /* if(module.hot){
     module.hot.accept()
 }   */ 
